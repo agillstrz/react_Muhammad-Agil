d66f3528029ec6f868534904abf6d261faf3968a
agillstrz <agillstrz@gmail.com>
2022-09-14 14:12:34 +0700

    Task/12_React-Fundamental : Complete

---
+++ 12_React Fundamental/Praktikum/todoapp/src/component/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <header className="border-bottom border-3 border-secondary p-3">
+        <h3>To Do App</h3>
+      </header>
+    </div>
+  );
+};
+export default Header;
